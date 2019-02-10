b2f816ab0bdfcd3ffe3c75f6ccde004e56e494bb
anuragde <anurag.dev@hotmail.com>
2019-02-09 22:29:17 -0500

    add springfox swagger for documentation

--- src/main/java/com/github/controller/ReadingsController.java
+++ src/main/java/com/github/controller/ReadingsController.java
@@ -2,6 +2,8 @@ package com.github.controller;
 
 import com.github.entity.Reading;
 import com.github.service.ReadingsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-
+@Api(description = "Endpoint to POST vehicle readings")
 @RestController
 public class ReadingsController {
 
@@ -20,6 +22,7 @@ public class ReadingsController {
 
     @RequestMapping(value = "/readings/", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
             consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiOperation(value = "inserts vehicle readings into database")
     public Reading create(@RequestBody Reading reading) {
         return readingsService.create(reading);
     }
--- src/main/java/com/github/controller/VehicleController.java
+++ src/main/java/com/github/controller/VehicleController.java
@@ -2,6 +2,8 @@ package com.github.controller;
 
 import com.github.entity.Vehicle;
 import com.github.service.VehicleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -11,32 +13,37 @@ import java.util.List;
 
 @Controller
 @RestController
+@Api(description = "Endpoint to manage vehicle details")
 public class VehicleController {
     @Autowired
     VehicleService vehicleService;
-
+    @ApiOperation(value = "Endpoint to insert a list of values into Database")
     @RequestMapping(method = RequestMethod.PUT, value = "/vehicles", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
             consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Vehicle> create(@RequestBody List<Vehicle> vehiclesList) {
         return vehicleService.create(vehiclesList);
     }
 
+    @ApiOperation(value = "Endpoint to update vehicle details by VIN")
     @RequestMapping(method = RequestMethod.PUT, value = "/vehicles/{vin}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
             consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Vehicle update(@PathVariable String vin, @RequestBody Vehicle vehicle) {
         return vehicleService.update(vin, vehicle);
     }
 
+    @ApiOperation(value = "Endpoint to get vehicle details by VIN")
     @RequestMapping(method = RequestMethod.GET, value = "/vehicles/{vin}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Vehicle findOne(@PathVariable String vin) {
         return vehicleService.findOne(vin);
     }
 
+    @ApiOperation(value = "Endpoint to get all vehicle details")
     @RequestMapping(method = RequestMethod.GET, value = "/vehicles", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Vehicle> findAll() {
         return vehicleService.findAll();
     }
 
+    @ApiOperation(value = "Endpoint to delete vehicle by VIN")
     @RequestMapping(method = RequestMethod.DELETE, value = "/vehicles/{vin}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public void delete(@PathVariable String vin) {
         vehicleService.delete(vin);
